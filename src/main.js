b86079d8323212601fed26a1e6fc7a5d53eecd5e
Liang <1377283637@qq.com>
2019-02-20 16:59:03 +0800

    第二次

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import api from './http/index'
+
+
+Vue.use(ElementUI);
+Vue.use(api)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/views/Login'
+import Home from '@/views/Home'
+import NotFound from '@/views/404'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: NotFound
+    },
   ]
 })
+
+// router.beforeEach((to, from, next) => {
+//   // 登录界面登录成功之后，会把用户信息保存在会话
+//   // 存在时间为会话生命周期，页面关闭即失效。
+//   let userName = sessionStorage.getItem('user')
+//   if (to.path === '/login') {
+//     // 如果是访问登录界面，如果用户会话信息存在，代表已登录过，跳转到主页
+//     if(userName) {
+//       next({ path: '/' })
+//     } else {
+//       next()
+//     }
+//   } else {
+//     if (!userName) {
+//       // 如果访问非登录界面，且户会话信息不存在，代表未登录，则跳转到登录界面
+//       next({ path: '/login' })
+//     } else {
+//       // 加载动态菜单和路由
+//       addDynamicMenuAndRoutes(userName, to, from)
+//       next()
+//     }
+//   }
+// })
+
+
+export default router
